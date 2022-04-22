c9e8db032ea4a07debf363e03dfa0bdcdf7e2976
USUI <usui-t2@asahi.com>
2022-04-22 16:17:57 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require("path")
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.post('/api/v1/quiz', function (req, res) {
+    const answer = req.body.answer;
+    if( answer === "2" ){
+        //res.send("<h1>正解</h1>")
+        res.redirect("/correct.html")
+    } else {
+        res.redirect("/wrong.html")
+        //res.send("<h1>フ正解</h1>")
+    }
+//    res.send(answer)
+})
+
+app.get('/', function (req, res) {
+    res.send('')
+})
+
+app.get('/about', function (req, res) {
+    res.send('Aboutページ')
+})
+
+app.listen(3000, function(){
+    console.log("I am running!")
+});
+
+console.log("最終行");
